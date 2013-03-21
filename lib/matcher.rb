5747f2cf1d65e60d807baa932f16a1890e8a2b19
Adam Hunter <adamhunter@me.com>
2013-03-20 22:16:17 -0400

    refactor algo

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -2,8 +2,11 @@ module Matcher
   extend self
 
   def flag(targets, compares)
-    f = compares.reduce(false) { |flag, compare| flag ||= targets.all? { |target| !match(target, compare) } }
-    targets.reduce(f) { |flag, target| flag ||= compares.all? { |compare| !match(target, compare) } }
+    compare(targets, compares) || compare(compares, targets)
+  end
+
+  def compare(targets, compares)
+    targets.reduce(false) { |flag, target| flag ||= compares.all? { |compare| !match(target, compare) } }
   end
 
   def match(target, compare)
